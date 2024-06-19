ef83671b646d2a94dbee8b715e35dce4c38cb77b
Vincent Prins <vincent.prins@ivdnt.org>
2024-06-19 15:17:57 +0200

    Simplified FileBackedValueTest
    Unfortunately, the previous test was non-deterministic. In the end, at the very least this tests reading and writing.

--- server/src/test/kotlin/org/ivdnt/galahad/FileBackedValueTest.kt
+++ server/src/test/kotlin/org/ivdnt/galahad/FileBackedValueTest.kt
@@ -16,13 +16,5 @@ internal class FileBackedValueTest {
         assertEquals("a string", fbv.read<String>())
         fbv.modify<String> { "some characters" }
         assertEquals("some characters", fbv.read<String>())
-
-        // Try to force some concurrent modifications
-        GlobalScope.launch { for (i in 0..100) fbv.modify<String> { "some characters" } }
-        for (i in 0..10) {
-            fbv.modify<String> { "some characters" }
-            fbv.read<String>()
-        }
-        assertEquals("some characters", fbv.read<String>())
     }
 }
