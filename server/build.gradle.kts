821617fc9d48c0fa6b539d9d89471d16f850bd68
Vincent Prins <vincent.prins@ivdnt.org>
2024-09-23 14:35:23 +0200

    Update gradle and dependencies

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -3,12 +3,12 @@ import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.2.3"
-	id("io.spring.dependency-management") version "1.1.4"
-	id("org.jetbrains.dokka") version "1.9.10"
-	kotlin("jvm") version "1.9.22"
-	kotlin("plugin.spring") version "1.9.22"
-	kotlin("plugin.serialization") version "1.9.22"
+	id("org.springframework.boot") version "3.3.4"
+	id("io.spring.dependency-management") version "1.1.6"
+	id("org.jetbrains.dokka") version "1.9.20"
+	kotlin("jvm") version "2.0.20"
+	kotlin("plugin.spring") version "2.0.20"
+	kotlin("plugin.serialization") version "2.0.20"
 }
 
 group = "org.ivdnt"
@@ -22,23 +22,23 @@ repositories {
 
 dependencies {
 	// Spring
-	implementation("org.springframework.boot:spring-boot-starter-web:3.2.4")
+	implementation("org.springframework.boot:spring-boot-starter-web:3.3.4")
 	// https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-devtools
-	implementation("org.springframework.boot:spring-boot-devtools:3.2.3")
+	implementation("org.springframework.boot:spring-boot-devtools:3.3.4")
 
 	// kotlin
 	implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.22")
 	// https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib
-	implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.20")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
 	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3") // JVM dependency
 
 	// swagger
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 
 
 	implementation("com.beust:klaxon:5.6")
-	implementation("org.apache.logging.log4j:log4j-api-kotlin:1.2.0")
+	implementation("org.apache.logging.log4j:log4j-api-kotlin:1.5.0")
 
 	// yaml
 	// https://mvnrepository.com/artifact/org.yaml/snakeyaml
