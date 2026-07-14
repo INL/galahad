f1ed1f2d2c07bf05181753541c1a75d7f5029e19
Vincent <146705420+PrinsINT@users.noreply.github.com>
2026-07-14 16:10:15 +0200

    Improve inline text button

--- client/src/components/input/button/InlineTextButton.vue
+++ client/src/components/input/button/InlineTextButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <code :class="classes"><slot></slot></code>
+    <span :class="classes"><slot></slot></span>
 </template>
 
 <script setup lang="ts">
@@ -13,9 +13,13 @@ const classes = { green: green, orange: orange, red: red, plain: plain }
 </script>
 
 <style scoped lang="scss">
-code {
+span {
+    display: inline-block;
     font-size: 0.8rem;
     padding: 0.2rem 0.3rem;
+    margin: 0.1rem 0;
+
+    background-color: var(--int-theme);
 
     &.green {
         background-color: var(--int-green);
@@ -31,6 +35,10 @@ code {
 
     &.plain {
         border: 1px solid var(--int-light-grey);
+    }
+
+    :deep(i.fa) {
+        font-size: 1rem;
     }
 }
 </style>
